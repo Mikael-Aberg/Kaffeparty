3d954ee0acdcea54c33832ce74c0217e7ef89484
Mikael Åberg <mikael.aberg@me.com>
2017-04-07 13:06:36 +0200

    Add project files.

---
+++ Kaffeparty/Scripts/scrolling.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    $('#submenu a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 120
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
